5cca08c305a82be12c004b423ac9f3ad366eed76
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-03-05 15:54:29 +0000

    Update ChangeString

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Text;
 
-namespace ChangeString
+namespace finalString
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            Console.Write("String: ");
+            string request1 = Console.ReadLine();
+            Console.Write("Caráter: ");
+            string request2 = Console.ReadLine();
+            char c2 = char.Parse(request2);
+
+            string final = "";
+
+            foreach (char c in request1)
+            {
+                if (c == c2)
+                {
+                    final += "x";
+                }
+
+                if (c != c2)
+                {
+                    final += c;
+                }
+            }
+            Console.WriteLine(final);
+
         }
     }
 }
